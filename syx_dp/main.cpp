e0af8ece077533146b3d611fdc057b90e683b93b
alvin <alvinsunyixiao@gmail.com>
2017-06-13 16:53:16 +0800

    finish syx_dp functionality + slight modification on dijkstra

--- syx_dp/main.cpp
+++ syx_dp/main.cpp
@@ -6,11 +6,17 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <cmath>
 
-#include "vertex.hpp"
-#include "edge.hpp"
-#include "vertexQueue.hpp"
+using namespace std;
 
+struct Position {
+    int x;
+    int y;
+    int trace_id;
+    double min_dis;
+    string label;
+};
 
 Position* start_point() {
     Position *p = new Position();
@@ -19,26 +25,15 @@ Position* start_point() {
     return p;
 }
 
-map<string, int> layout;
-
-void dijkstra(VertexQueue &vqueue) {
-    while (vqueue.size() > 0) {
-        Vertex *current = vqueue.pop_front();
-        current->marked = true;
-        for (size_t i=0;i<current->target.size();i++) {
-            if (current->target[i]->to->marked)
-                continue;
-            if (current->target[i]->to->queue_idx == -1)
-                vqueue.insert(current->target[i]);
-            else
-                vqueue.decrement(current->target[i]->to->queue_idx, current->target[i]);
-        }
-    }
+double dis(Position &a, Position &b) {
+    return sqrt(pow(a.x-b.x, 2) + pow(a.y-b.y, 2));
 }
+
+map<string, int> layout;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        cout << "There should be exactly two parameter!" << endl;
+        cout << "There should be exactly one parameter!" << endl;
         return 0;
     }
 
@@ -68,12 +63,12 @@ int main(int argc, char *argv[]) {
         getline(ss, l, ',');
         l.pop_back();
         cout << x << ' ' << y << ' ' << l << endl;
-        buf[layout[l]].push_back({x, y});
+        buf[layout[l]].push_back({x, y, -1, 0, l});
     }
 
-    vector<vector<Vertex*>> graph;
-    vector<Vertex*> start;
-    start.push_back(new Vertex(start_point(), "Start"));
+    vector<vector<Position>> graph;
+    vector<Position> start;
+    start.push_back({0,0,-1,0,"Start"});
     graph.push_back(start);
 
     while (getline(guide_fin, tmp, ',')) {
@@ -81,34 +76,35 @@ int main(int argc, char *argv[]) {
             tmp.pop_back();
             tmp.pop_back();
         }
-        vector<Vertex*> row;
+        vector<Position> row;
         for (size_t i=0; i<buf[layout[tmp]].size(); i++) {
-            row.push_back(new Vertex(&(buf[layout[tmp]][i]), tmp));
+            Position x = buf[layout[tmp]][i];
+            x.min_dis = graph.back()[0].min_dis + dis(x, graph.back()[0]);
             for (size_t j=0; j<graph.back().size(); j++) {
-                graph.back()[j]->target.push_back(new Edge(graph.back()[j], row.back()));
+                if (graph.back()[j].min_dis + dis(x, graph.back()[j]) <= x.min_dis) {
+                    x.min_dis = graph.back()[j].min_dis + dis(x, graph.back()[j]);
+                    x.trace_id = j;
+                }
             }
+            row.push_back(x);
         }
         graph.push_back(row);
     }
 
-    VertexQueue vqueue;
-    start[0]->dis = 0;
-    start[0]->queue_idx = 0;
-    vqueue.data.push_back(start[0]);
-
-    dijkstra(vqueue);
-
-    Vertex *end;
-    double min_dis = 10000000000;
+    Position end = graph.back()[0];
+    double min_dis = end.min_dis;
     for (size_t i=0; i<graph.back().size(); i++) {
-        if (graph.back()[i]->dis < min_dis) {
+        if (min_dis > graph.back()[i].min_dis) {
             end = graph.back()[i];
-            min_dis = graph.back()[i]->dis;
+            min_dis = graph.back()[i].min_dis;
         }
     }
-    while(end) {
-        printf("(%d, %d, %s) ", end->pos->x, end->pos->y, end->label.c_str());
-        end = end->parent;
+
+    size_t level = graph.size()-1;
+    while (end.trace_id != -1) {
+        level--;
+        printf("(%d, %d, %s) ", end.x, end.y, end.label.c_str());
+        end = graph[level][end.trace_id];
     }
 
     return 0;
