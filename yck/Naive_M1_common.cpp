fdb3f8a499700e4925d42bfcef224c47af202aa9
alvin <alvinsunyixiao@gmail.com>
2017-06-17 14:14:03 +0800

    update yck

---
+++ yck/Naive_M1_common.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+const int       nmax = 2048;                    //maximum number of trees
+const int       ccnt = 8;                       //number of tree types
+const double    inf = 1e40;
+
+struct  Position                                //tree representation
+{
+    int x;
+    int y;
+    int c;                                      //type ID
+
+    Position(int x = 0, int y = 0, int c = 0)
+    {
+        this->x = x;
+        this->y = y;
+        this->c = c;
+    }
+}       T[nmax];
+
+int     n;                                      //number of trees
+int     step[nmax], sn;                         //type sequence, type length
+int     bcnt[ccnt], bucket[ccnt][nmax];         //tree categorization
+int     f[nmax][nmax];                          //trace-back ID
+int     route[nmax];
+int     end_idx;
+double  d[nmax][nmax];                          //optimal distance
+double  mn_d = inf;
+double  dist[nmax][nmax];                       //distance between two trees
+
+double dis(Position &a, Position &b)
+{
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
+
+void init(string root = "./")
+{
+    map <string, int> m;
+    m["Cedar"]          = 1;
+    m["PlaneTree"]      = 2;
+    m["Palm"]           = 3;
+    m["Pine"]           = 4;
+    m["MaidenhairTree"] = 5;
+    m["Birch"]          = 6;
+    m["Polar"]          = 7;
+
+    int x, y;
+    char name[20];
+
+    FILE *fm = fopen((root + "/map.csv").c_str(), "r");
+    while (fscanf(fm, "%d,%d,%s", &x, &y, name) != EOF)
+        T[++n] = Position(x, y, m[name]);
+
+    fstream fg(root + "/guidebook.csv");
+    while (fg.getline(name, 100, ','))
+    {
+        int l = strlen(name), i;
+        for (i = 0; i < l; i++)
+            if (name[i] < 'A' || name[i] > 'Z' && name[i] < 'a' || name[i] > 'z')
+                break;
+        name[i] = 0;
+        step[++sn] = m[name];
+    }
+
+    //preprocess all possible distances
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= n; j++)
+            dist[i][j] = dis(T[i], T[j]);
+
+    //categorize trees
+    for (int i = 1; i <= n; i++)
+        bucket[T[i].c][++bcnt[T[i].c]] = i;
+}
+
+void DP()
+{
+    int i, j, k, x, y;
+
+    //initialize all optimal distances to infinity
+    for (i = 0; i <= sn; i++)
+        for (j = 0; j <= n; j++)
+            d[i][j] = inf;
+
+    //calculate the distances of the first step
+    for (j = 1; j <= n; j++)
+        d[1][j] = dist[0][j],
+        f[1][j] = 0;
+    //calculate the distances of the following steps
+    for (i = 2; i <= sn; i++)
+        for (x = 1; x <= bcnt[step[i]]; x++)                //iterate over all trees of the current type
+        {
+            j = bucket[step[i]][x];
+            for (y = 1; y <= bcnt[step[i - 1]]; y++)        //iterate over all trees of the previous type
+            {
+                k = bucket[step[i - 1]][y];
+                if (d[i][j] > d[i - 1][k] + dist[j][k])
+                    d[i][j] = d[i - 1][k] + dist[j][k],
+                    f[i][j] = k;
+            }
+        }
+
+    //find the optimal solution in the final step
+    for (j = 1; j <= n; j++)
+        if (step[sn] == T[j].c && mn_d > d[sn][j])
+            mn_d = d[sn][j],
+            end_idx = j;
+}
+
+void output()
+{
+    string str[8] = {"", "Cedar", "PlaneTree", "Palm", "Pine", "MaidenhairTree", "Birch", "Polar"};
+    char name[20], file[50];
+
+    sprintf(file, "1-solution-%d.csv", int(mn_d + 0.5));
+    FILE *out = fopen(file, "w");
+    int t = end_idx;
+    for (int i = sn; i; t = f[i--][t])
+        route[i] = t;
+    for (int i = 1; i <= sn; i++)
+    {
+        str[T[route[i]].c].copy(name, str[T[route[i]].c].length());
+        name[str[T[route[i]].c].length()] = 0;
+        fprintf(out, "%d,%d,%s\n", T[route[i]].x, T[route[i]].y, name);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        init();
+    else
+        init(string(argv[1]));
+    DP();
+    output();
+    return 0;
+}
--- yck/Tactful_M2.cpp
+++ yck/Tactful_M2.cpp
@@ -13,23 +13,23 @@ const double inf = 1e40;
 
 struct Position
 {
-	int x, y, c;
-	Position(int x = 0, int y = 0, int c = 0)
-	{
-		this->x = x;
-		this->y = y;
-		this->c = c;
-	}
+    int x, y, c;
+    Position(int x = 0, int y = 0, int c = 0)
+    {
+        this->x = x;
+        this->y = y;
+        this->c = c;
+    }
 } T[nmax];
 
 bool prob(double p)
 {
-	//if (rand() == 0) printf("%lf ", p);
-	unsigned int x = ((rand() << 15) + rand()) % (1 << 30);
-	if (x < p * (1 << 30))
-		return 1;
-	else
-		return 0;
+    //if (rand() == 0) printf("%lf ", p);
+    unsigned int x = ((rand() << 15) + rand()) % (1 << 30);
+    if (x < p * (1 << 30))
+        return 1;
+    else
+        return 0;
 }
 
 int n, step[nmax], sn, bcnt[8], bucket[8][nmax];
@@ -39,17 +39,17 @@ int gsize;
 vector<int> grid[8][32][32];
 void neighbor_of(int t, int &tar)
 {
-	int c = T[t].c, x = T[t].x / gsize, y = T[t].y / gsize, tmp = 0;
-	for (int i = x == 0 ? 0 : x - 1; i <= x + 1; i++)
-		for (int j = y == 0 ? 0 : y - 1; j <= y + 1; j++)
-			for (int k = 0; k < grid[c][i][j].size(); k++)
-				if (rand() % ++tmp == 0)
-					tar = grid[c][i][j][k];
+    int c = T[t].c, x = T[t].x / gsize, y = T[t].y / gsize, tmp = 0;
+    for (int i = x == 0 ? 0 : x - 1; i <= x + 1; i++)
+        for (int j = y == 0 ? 0 : y - 1; j <= y + 1; j++)
+            for (int k = 0; k < grid[c][i][j].size(); k++)
+                if (rand() % ++tmp == 0)
+                    tar = grid[c][i][j][k];
 }
 
 double dis(Position &a, Position &b)
 {
-	return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
 double mn_d;
@@ -57,174 +57,174 @@ int route[nmax], mn_i;
 bool vis[nmax];
 void work()
 {
-	int i, j, k;
-	for (i = 1; i <= sn; i++)
-	{
-		mn_d = inf;
-		for (j = 1; j <= bcnt[step[i]]; j++)
-		{
-			k = bucket[step[i]][j];
-			if (!vis[k] && mn_d > dis(T[route[i - 1]], T[k]))
-				mn_d = dis(T[route[i - 1]], T[k]),
-				mn_i = k;
-		}
-		route[i] = mn_i;
-		vis[mn_i] = 1;
-	}
+    int i, j, k;
+    for (i = 1; i <= sn; i++)
+    {
+        mn_d = inf;
+        for (j = 1; j <= bcnt[step[i]]; j++)
+        {
+            k = bucket[step[i]][j];
+            if (!vis[k] && mn_d > dis(T[route[i - 1]], T[k]))
+                mn_d = dis(T[route[i - 1]], T[k]),
+                mn_i = k;
+        }
+        route[i] = mn_i;
+        vis[mn_i] = 1;
+    }
 }
 
 struct Particle
 {
-	int sq[nmax], cnt[nmax], last_id;
-	double e_change;
-	Particle()
-	{
-		work();
-		for (int i = 0; i <= n; i++)
-			cnt[i] = 0;
-		for (int i = 1; i <= sn; i++)
-			cnt[sq[i] = route[i]]++;
-		last_id = -1;
-		e_change = 0;
-	}
-	void change(double tem, double alpha = 0)
-	{
-		int id = rand() % sn + 1;
-		if (last_id > 1 && last_id < sn)
-		{
-			if (rand() & 1) id = last_id + 1;
-			else id = last_id - 1;
-		}
-		
-		int tar = sq[id];//bucket[T[sq[id]].c][rand() % bcnt[T[sq[id]].c] + 1];
-		neighbor_of(sq[id], tar);
-		
-		double delta_e = alpha * ((cnt[tar] >= 1) - (cnt[sq[id]] >= 2));
-		if (id != 1)  delta_e += dist[tar][sq[id - 1]] - dist[sq[id]][sq[id - 1]];
-		if (id != sn) delta_e += dist[tar][sq[id + 1]] - dist[sq[id]][sq[id + 1]];
-		
-		if (delta_e <= 0 || prob(exp(-delta_e / tem)))
-		{
-			cnt[tar]++;
-			cnt[sq[id]]--;
-			sq[id] = tar;
-			last_id = id;
-			e_change += delta_e;
-			//if (tem < 12 && rand() < 10) printf(" ");
-		}
-		else last_id = -1;
-	}
-	void output()
-	{
-		for (int i = 1; i <= sn; i++)
-			printf("%d ", sq[i]);
-		printf("\n");
-	}
+    int sq[nmax], cnt[nmax], last_id;
+    double e_change;
+    Particle()
+    {
+        work();
+        for (int i = 0; i <= n; i++)
+            cnt[i] = 0;
+        for (int i = 1; i <= sn; i++)
+            cnt[sq[i] = route[i]]++;
+        last_id = -1;
+        e_change = 0;
+    }
+    void change(double tem, double alpha = 0)
+    {
+        int id = rand() % sn + 1;
+        if (last_id > 1 && last_id < sn)
+        {
+            if (rand() & 1) id = last_id + 1;
+            else id = last_id - 1;
+        }
+
+        int tar = sq[id];//bucket[T[sq[id]].c][rand() % bcnt[T[sq[id]].c] + 1];
+        neighbor_of(sq[id], tar);
+
+        double delta_e = alpha * ((cnt[tar] >= 1) - (cnt[sq[id]] >= 2));
+        if (id != 1)  delta_e += dist[tar][sq[id - 1]] - dist[sq[id]][sq[id - 1]];
+        if (id != sn) delta_e += dist[tar][sq[id + 1]] - dist[sq[id]][sq[id + 1]];
+
+        if (delta_e <= 0 || prob(exp(-delta_e / tem)))
+        {
+            cnt[tar]++;
+            cnt[sq[id]]--;
+            sq[id] = tar;
+            last_id = id;
+            e_change += delta_e;
+            //if (tem < 12 && rand() < 10) printf(" ");
+        }
+        else last_id = -1;
+    }
+    void output()
+    {
+        for (int i = 1; i <= sn; i++)
+            printf("%d ", sq[i]);
+        printf("\n");
+    }
 };
 
 void init()
 {
-	map <string, int> m;
-	m["Cedar"] = 1;
-	m["PlaneTree"] = 2;
-	m["Palm"] = 3;
-	m["Pine"] = 4;
-	m["MaidenhairTree"] = 5;
-	m["Birch"] = 6;
-	m["Polar"] = 7;
-	
-	int x, y;
-	char name[20];
-	
-	FILE *fm = fopen("map.csv", "r");
-	while (fscanf(fm, "%d,%d,%s", &x, &y, name) != EOF)
-		T[++n] = Position(x, y, m[name]);
-	
-	fstream fg("guidebook.csv");
-	while (fg.getline(name, 100, ','))
-	{
-		int l = strlen(name), i;
-		for (i = 0; i < l; i++)
-			if (name[i] < 'A' || name[i] > 'Z' && name[i] < 'a' || name[i] > 'z')
-				break;
-		name[i] = 0;
-		step[++sn] = m[name];
-	}
-	
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			dist[i][j] = dis(T[i], T[j]);
-	
-	for (int i = 1; i <= n; i++)
-		bucket[T[i].c][++bcnt[T[i].c]] = i;
-	
-	gsize = sqrt(n) * 3;
-	for (int i = 1; i <= n; i++)
-		grid[T[i].c][T[i].x / gsize][T[i].y / gsize].push_back(i);
+    map <string, int> m;
+    m["Cedar"] = 1;
+    m["PlaneTree"] = 2;
+    m["Palm"] = 3;
+    m["Pine"] = 4;
+    m["MaidenhairTree"] = 5;
+    m["Birch"] = 6;
+    m["Polar"] = 7;
+
+    int x, y;
+    char name[20];
+
+    FILE *fm = fopen("map.csv", "r");
+    while (fscanf(fm, "%d,%d,%s", &x, &y, name) != EOF)
+        T[++n] = Position(x, y, m[name]);
+
+    fstream fg("guidebook.csv");
+    while (fg.getline(name, 100, ','))
+    {
+        int l = strlen(name), i;
+        for (i = 0; i < l; i++)
+            if (name[i] < 'A' || name[i] > 'Z' && name[i] < 'a' || name[i] > 'z')
+                break;
+        name[i] = 0;
+        step[++sn] = m[name];
+    }
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            dist[i][j] = dis(T[i], T[j]);
+
+    for (int i = 1; i <= n; i++)
+        bucket[T[i].c][++bcnt[T[i].c]] = i;
+
+    gsize = sqrt(n) * 3;
+    for (int i = 1; i <= n; i++)
+        grid[T[i].c][T[i].x / gsize][T[i].y / gsize].push_back(i);
 }
 
 Particle p;
 void SA()
 {
-	p = Particle();
-	
-	double ts = 30, te = 1, last_pc = 0, pc, tot = log(ts / te), alpha = 1000;
-	for (double tem = ts; tem > te; tem *= 0.99999999)
-	{
-		p.change(tem, alpha);
-		pc = log(ts / tem) / tot;
-		if (pc - last_pc > 0.01)
-		{
-			printf("%d%%. E change is %lf\n", int(pc * 100), p.e_change);
-			last_pc = pc;
-			alpha *= 10;
-		}
-	}
-	
-	//p.output();
+    p = Particle();
+
+    double ts = 30, te = 1, last_pc = 0, pc, tot = log(ts / te), alpha = 1000;
+    for (double tem = ts; tem > te; tem *= 0.99999999)
+    {
+        p.change(tem, alpha);
+        pc = log(ts / tem) / tot;
+        if (pc - last_pc > 0.01)
+        {
+            printf("%d%%. E change is %lf\n", int(pc * 100), p.e_change);
+            last_pc = pc;
+            alpha *= 10;
+        }
+    }
+
+    //p.output();
 }
 
 void output(double length)
 {
-	string str[8] = {"", "Cedar", "PlaneTree", "Palm", "Pine", "MaidenhairTree", "Birch", "Polar"};
-	char name[20], file[50];
-	
-	sprintf(file, "2L-solution-%d.csv", int(length + 0.5));
-	FILE *out = fopen(file, "w");
-	for (int i = 1; i <= sn; i++)
-	{
-		str[T[route[i]].c].copy(name, str[T[route[i]].c].length());
-		name[str[T[route[i]].c].length()] = 0;
-		fprintf(out, "%d,%d,%s\n", T[route[i]].x, T[route[i]].y, name);
-	}
+    string str[8] = {"", "Cedar", "PlaneTree", "Palm", "Pine", "MaidenhairTree", "Birch", "Polar"};
+    char name[20], file[50];
+
+    sprintf(file, "2L-solution-%d.csv", int(length + 0.5));
+    FILE *out = fopen(file, "w");
+    for (int i = 1; i <= sn; i++)
+    {
+        str[T[route[i]].c].copy(name, str[T[route[i]].c].length());
+        name[str[T[route[i]].c].length()] = 0;
+        fprintf(out, "%d,%d,%s\n", T[route[i]].x, T[route[i]].y, name);
+    }
 }
 
 int main()
 {
-	srand(time(0));
-	
-	init();
-	
-	double length, mn_l = inf;
-	Particle *opt;
-	for (int t = 0; t < 1; t++)
-	{
-		printf("%d\n", t);
-		SA();
-		length = 0;
-		for (int i = 1; i <= sn; i++)
-			length += dis(T[p.sq[i - 1]], T[p.sq[i]]);
-		if (mn_l > length)
-		{
-			mn_l = length;
-			opt = &p;
-		}
-	}
-	
-	for (int i = 1; i <= sn; i++)
-		route[i] = opt->sq[i];
-	
-	output(mn_l);
-	return 0;
+    srand(time(0));
+
+    init();
+
+    double length, mn_l = inf;
+    Particle *opt;
+    for (int t = 0; t < 1; t++)
+    {
+        printf("%d\n", t);
+        SA();
+        length = 0;
+        for (int i = 1; i <= sn; i++)
+            length += dis(T[p.sq[i - 1]], T[p.sq[i]]);
+        if (mn_l > length)
+        {
+            mn_l = length;
+            opt = &p;
+        }
+    }
+
+    for (int i = 1; i <= sn; i++)
+        route[i] = opt->sq[i];
+
+    output(mn_l);
+    return 0;
 }
 
